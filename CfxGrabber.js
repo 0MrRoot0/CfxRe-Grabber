92cf68e6da9e9d924beccf41b57b00664b07e728
0MrRoot0 <82186529+0MrRoot0@users.noreply.github.com>
2021-12-21 22:22:51 +0200

    Add files via upload

---
+++ CfxGrabber.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; /*Parempi vaan constiin pivitt*/
+rl.question('CFX.RE URL : ', function (name) {
+    	var req = new XMLHttpRequest();
+	req.open('GET', "https://"+name, false);
+	req.send(null);
+	var headers = req.getResponseHeader('x-citizenfx-url')
+	rep_one = headers.replace("http://","");
+	rep_two = rep_one.replace(":30120","");
+	rep_done = rep_two.replace("/","");
+	console.log("IP EXTRACTED :" + rep_done);   
+    	rl.close();
+});	
